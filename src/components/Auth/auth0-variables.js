0ca373407589062acdcab4e5a52fa06e725d765f
marcelines <marcio@significa.co>
2021-04-26 10:15:22 +0100

    Update again

--- src/components/Auth/auth0-variables.js
+++ src/components/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
-  domain: 'graphql-tutorials.auth0.com',
-  clientId: 'P38qnFo1lFAQJrzkun--wEzqljVNGcWW',
-  callbackUrl: 'https://hasura-todos-boilerplate.herokuapp.com//callback',
+  domain: 'hasura-marcelines-backend.eu.auth0.com',
+  clientId: 'K0j6PNrmzYjHNRmJaVedwADgu7IDi0FC',
+  callbackUrl: 'https://hasura-todos-boilerplate.herokuapp.com/callback',
   afterLogout: 'https://hasura-todos-boilerplate.herokuapp.com/'
 };
